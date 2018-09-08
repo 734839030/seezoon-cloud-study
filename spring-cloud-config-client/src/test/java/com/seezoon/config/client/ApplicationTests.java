2ec69685964a9343dbaff724643a6da653a499b7
734839030 <734839030@qq.com>
2018-09-08 22:17:04 +0800

    config

--- spring-cloud-config-client/src/test/java/com/seezoon/config/client/ApplicationTests.java
+++ spring-cloud-config-client/src/test/java/com/seezoon/config/client/ApplicationTests.java
@@ -1,10 +1,13 @@
 
 package com.seezoon.config.client;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -19,4 +22,10 @@ public  class ApplicationTests {
 	 * 日志对象
 	 */
 	protected Logger logger = LoggerFactory.getLogger(getClass());
+	@Autowired
+	private Environment environment;
+	@Test
+	public void t1() {
+		System.out.println(environment.getProperty("author.name"));
+	}
 }
